701be7cb21d63fed0ce230b5d04c81f3d709afbb
Allan-12 <hei.allan.3@gmail.com>
2025-02-05 10:46:12 +0300

    chore: added HelloWorldController

--- src/main/java/com/example/demo/endpoint/rest/controller/health/HelloWorld.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/health/HelloWorld.java
@@ -1,14 +1,13 @@
 package com.example.demo.endpoint.rest.controller.health;
 
+import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 @RestController
 public class HelloWorld {
-    @GetMapping("/hello")
-    public Map<String,String> sayHello(){
-        return Map.of("message","Hello Wordl");
-    }
+  @GetMapping("/hello")
+  public Map<String, String> sayHello() {
+    return Map.of("message", "Hello Wordl");
+  }
 }
